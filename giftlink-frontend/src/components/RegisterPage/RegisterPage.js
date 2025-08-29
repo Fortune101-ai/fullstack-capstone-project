2f9ea7a8e950f38ba29a56233d256ffd18c6e1f2
Fortune Mahloane <sensefortune01@gmail.com>
2025-08-29 21:03:59 +0200

    update backend registration code

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,20 +1,53 @@
 import React, { useState } from "react";
+import { urlConfig } from "../../config";
+import { useAppContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 import "./RegisterPage.css";
 
 function RegisterPage() {
-  //insert code here to create useState hook variables for firstName, lastName, email, password
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  // insert code here to create handleRegister function and include console.log
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    console.log(
-      `Registering user with details:,  firstName: ${firstName}, lastName: ${lastName}, email: ${email}`
-    );
+  const [showerr, setShowerr] = useState("");
+
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useAppContext();
+
+  const handleRegister = async () => {
+    const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+      method: "POST",
+
+      headers: {
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      }),
+    });
+
+    const json = await response.json();
+    console.log("json data", json);
+    console.log("er", json.error);
+
+    if (json.authtoken) {
+      sessionStorage.setItem("auth-token", json.authtoken);
+      sessionStorage.setItem("name", firstName);
+      sessionStorage.setItem("email", json.email);
+
+      setIsLoggedIn(true);
+
+      navigate("/app");
+    }
+    if (json.error) {
+      setShowerr(json.error);
+    }
   };
 
   return (
@@ -23,14 +56,10 @@ function RegisterPage() {
         <div className="col-md-6 col-lg-4">
           <div className="register-card p-4 border rounded">
             <h2 className="text-center mb-4 font-weight-bold">Register</h2>
-
-            {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
-            <div className="mb-4">
-              <label htmlFor="firstName" className="form label">
-                {" "}
+            <div className="mb-3">
+              <label htmlFor="firstName" className="form-label">
                 FirstName
               </label>
-              <br />
               <input
                 id="firstName"
                 type="text"
@@ -41,7 +70,53 @@ function RegisterPage() {
               />
             </div>
 
-            {/* insert code here to create a button that performs the `handleRegister` function on click */}
+            {/* last name */}
+
+            <div className="mb-3">
+              <label htmlFor="lastName" className="form-label">
+                LastName
+              </label>
+              <input
+                id="lastName"
+                type="text"
+                className="form-control"
+                placeholder="Enter your lastName"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+
+            {/* email  */}
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                id="email"
+                type="text"
+                className="form-control"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {/* Step 2 - Task 6*/}
+
+              <div className="text-danger">{showerr}</div>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
             <button
               className="btn btn-primary w-100 mb-3"
               onClick={handleRegister}
@@ -58,7 +133,7 @@ function RegisterPage() {
         </div>
       </div>
     </div>
-  ); //end of return
+  );
 }
 
 export default RegisterPage;
